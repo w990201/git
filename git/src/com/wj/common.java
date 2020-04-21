38d495cf58791ec4e2e62280dada035fbc44fe97
wj <1842976155@qq.com>
2020-04-21 12:41:30 +0800

    把本地工程添加到git

---
+++ git/src/com/wj/common.java
@@ -0,0 +1,10 @@
+package com.wj;
+
+public class common {
+    public static void main(String[] args) {
+        System.out.println("sssss");
+        System.out.println("sssss");
+        System.out.println("sssss");
+        System.out.println("sssss");
+    }
+}
